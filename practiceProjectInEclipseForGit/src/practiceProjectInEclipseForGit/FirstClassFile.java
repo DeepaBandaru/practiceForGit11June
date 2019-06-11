1013802edbbb83fb7bfe123b602a1ae579272120
DeepaBandaru <bandaru.deepa14@gmail.com>
2019-06-11 10:47:10 +0800

    Created new project for the demo on git

---
+++ practiceProjectInEclipseForGit/src/practiceProjectInEclipseForGit/FirstClassFile.java
@@ -0,0 +1,10 @@
+package practiceProjectInEclipseForGit;
+
+public class FirstClassFile {
+	
+	public static void main(String arga[]) {
+		
+		// this is code for first class for Demo on Git
+	}
+
+}
